6a7af3d948d2da3e1a61a7072b10349c8defd90b
tadahiro.wada <tadahiro.wada@d8aspring.com>
2017-09-14 10:38:15 +0900

    i_10 object expression

--- src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -18,6 +18,13 @@ fun todoTask10(): Nothing = TODO(
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
+    val comparator: Comparator<Int> = object: Comparator<Int> {
+        override fun compare(o1: Int, o2: Int): Int {
+            return o2 - o1
+        }
+
+    }
+    Collections.sort(arrayList, comparator)
     return arrayList
 }
+
