1295f097b998eb9a3e052f270bbd5ed7ec8b1393
tadahiro.wada <tadahiro.wada@d8aspring.com>
2017-09-22 10:01:45 +0900

    iii_25 comparison

--- src/iii_conventions/n25Comparison.kt
+++ src/iii_conventions/n25Comparison.kt
@@ -16,7 +16,12 @@ fun todoTask25(): Nothing = TODO(
 )
 
 fun task25(date1: MyDate, date2: MyDate): Boolean {
-    todoTask25()
-//    return date1 < date2
+    //todoTask25()
+    return date1 < date2
+}
+
+operator fun MyDate.compareTo(other: MyDate): Int {
+    return (this.year.toString() + this.month.toString() + this.dayOfMonth.toString()).toInt() -
+            (other.year.toString() + other.month.toString() + other.dayOfMonth.toString()).toInt()
 }
 
